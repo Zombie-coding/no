c6779e355badb0c881d64aa65f6d99b51598c260
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-17 14:15:48 +0300

    Update FilterManufacturerAccordion.tsx

--- components/modules/CatalogPage/FilterManufacturerAccordion.tsx
+++ components/modules/CatalogPage/FilterManufacturerAccordion.tsx
@@ -34,7 +34,7 @@ const FilterManufacturerAccordion = ({
           className={styles.filters__manufacturer__select_all}
           onClick={chooseAllManufacturers}
         >
-          Выбрать все
+          Valige kõik
         </button>
         <ul className={styles.filters__manufacturer__list}>
           {manufacturersList.map((item) => (

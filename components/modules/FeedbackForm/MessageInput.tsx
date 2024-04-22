ae2c02ce30ef565b9fbe3a47393c0b0ec6701906
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-22 21:38:50 +0300

    Update MessageInput.tsx

--- components/modules/FeedbackForm/MessageInput.tsx
+++ components/modules/FeedbackForm/MessageInput.tsx
@@ -5,9 +5,9 @@ const MessageInput = ({ register, errors, darkModeClass }: IFeedbackInput) => (
   <label className={`${styles.feedback_form__form__label} ${darkModeClass}`}>
     <textarea
       className={`${styles.feedback_form__form__textarea} ${darkModeClass}`}
-      placeholder="Sisestage oma sõnum (20 kuni 300 tähemärki)"
+      placeholder="Введите ваше сообщение (от 20 до 300 символов)"
       {...register('message', {
-        required: 'Sisestage sõnum!',
+        required: 'Введите сообщение!',
         minLength: 20,
         maxLength: 300,
       })}
@@ -16,10 +16,10 @@ const MessageInput = ({ register, errors, darkModeClass }: IFeedbackInput) => (
       <span className={styles.error_alert}>{errors.message?.message}</span>
     )}
     {errors.message && errors.message.type === 'minLength' && (
-      <span className={styles.error_alert}>Vähemalt 20 tähemärki!</span>
+      <span className={styles.error_alert}>Минимум 20 символов!</span>
     )}
     {errors.message && errors.message.type === 'maxLength' && (
-      <span className={styles.error_alert}>Mitte rohkem kui 300 tähemärki!</span>
+      <span className={styles.error_alert}>Не более 300 символов!</span>
     )}
   </label>
 )

2af189f2ce9ca94d3ebb7f5a535dd1f9d3b2c70a
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-22 14:17:53 +0300

    Update CartAlert.tsx

--- components/modules/DashboardPage/CartAlert.tsx
+++ components/modules/DashboardPage/CartAlert.tsx
@@ -13,30 +13,30 @@ const CartAlert = ({ count, closeAlert }: ICartAlertProps) => {
 
   const showCountMessage = (count: string) => {
     if (count.endsWith('1')) {
-      return 'товар'
+      return 'kaup'
     }
 
     if (count.endsWith('2') || count.endsWith('3') || count.endsWith('4')) {
-      return 'товара'
+      return 'kaubad'
     }
 
-    return 'товаров'
+    return 'Kaubad'
   }
 
   return (
     <>
       <div className={`${styles.dashboard__alert__left} ${darkModeClass}`}>
         <span>
-          В корзине {count} {showCountMessage(`${count}`)}
+         Korvis {count} {showCountMessage(`${count}`)}
         </span>
-        <span>На сумму {formatPrice(totalPrice)} P</span>
+        <span>Summa eest {formatPrice(totalPrice)} €</span>
       </div>
       <div className={styles.dashboard__alert__right}>
         <Link href="/order" legacyBehavior passHref>
-          <a className={styles.dashboard__alert__btn_cart}>Перейти в корзину</a>
+          <a className={styles.dashboard__alert__btn_cart}>Mine ostukorvi</a>
         </Link>
         <Link href="/order" legacyBehavior passHref>
-          <a className={styles.dashboard__alert__btn_order}>Оформить заказ</a>
+          <a className={styles.dashboard__alert__btn_order}>Tee tellimus</a>
         </Link>
       </div>
       <button

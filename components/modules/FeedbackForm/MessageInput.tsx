098dbc9a69e3fd4a0bcd6a92e2ba9b0e79985903
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-22 21:44:26 +0300

    Update MessageInput.tsx

--- components/modules/FeedbackForm/MessageInput.tsx
+++ components/modules/FeedbackForm/MessageInput.tsx
@@ -5,7 +5,7 @@ const MessageInput = ({ register, errors, darkModeClass }: IFeedbackInput) => (
   <label className={`${styles.feedback_form__form__label} ${darkModeClass}`}>
     <textarea
       className={`${styles.feedback_form__form__textarea} ${darkModeClass}`}
-      placeholder="Sisestage oma sõnum (20 kuni 300 tähemärki)"
+      placeholder="Введите ваше сообщение (от 20 до 300 символов)"
       {...register('message', {
         required: 'Sisestage sõnum!',
         minLength: 20,

5a90775ca672a961673253584df1bfa4052b9a37
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-22 14:02:47 +0300

    Update BrandsSlider.tsx

--- components/modules/DashboardPage/BrandsSlider.tsx
+++ components/modules/DashboardPage/BrandsSlider.tsx
@@ -16,18 +16,18 @@ const BrandsSlider = () => {
   const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
 
   const brandItems = [
-    { id: 1, img: '/img/brand-1.png', alt: 'brand-1' },
-    { id: 2, img: '/img/brand-3.png', alt: 'brand-3' },
-    { id: 3, img: '/img/brand-2.svg', alt: 'brand-2' },
-    { id: 4, img: '/img/brand-4.png', alt: 'brand-4' },
+    { id: 1, img: '/img/brand-7.png', alt: 'brand-1' },
+    { id: 2, img: '/img/brand-8.png', alt: 'brand-3' },
+    { id: 3, img: '/img/brand-9.png', alt: 'brand-2' },
+    { id: 4, img: '/img/brand-6.png', alt: 'brand-4' },
     { id: 5, img: '/img/brand-1.png', alt: 'brand-1' },
     { id: 6, img: '/img/brand-3.png', alt: 'brand-3' },
-    { id: 7, img: '/img/brand-2.svg', alt: 'brand-2' },
-    { id: 8, img: '/img/brand-1.png', alt: 'brand-1' },
-    { id: 9, img: '/img/brand-3.png', alt: 'brand-3' },
-    { id: 10, img: '/img/brand-4.png', alt: 'brand-4' },
-    { id: 11, img: '/img/brand-2.svg', alt: 'brand-2' },
-    { id: 12, img: '/img/brand-1.png', alt: 'brand-1' },
+    { id: 7, img: '/img/brand-9.png', alt: 'brand-2' },
+    { id: 8, img: '/img/brand-10.png', alt: 'brand-1' },
+    { id: 9, img: '/img/brand-11.png', alt: 'brand-3' },
+    { id: 10, img: '/img/brand-6.png', alt: 'brand-4' },
+    { id: 11, img: '/img/brand-12.png', alt: 'brand-2' },
+    { id: 12, img: '/img/brand-10.png', alt: 'brand-1' },
   ]
 
   useEffect(() => {

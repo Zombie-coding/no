6b5cf83ab4710972bc27113f90b4080f81aa0e05
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-23 21:07:04 +0300

    Update AboutPage.tsx

--- components/templates/AboutPage/AboutPage.tsx
+++ components/templates/AboutPage/AboutPage.tsx
@@ -16,7 +16,7 @@ const AboutPage = () => {
         <div className={styles.about__inner}>
           <div className={`${styles.about__info} ${darkModeClass}`}>
             <p>
-              E-pood Voinikoo.ee &quot;АкваТермикс&quot; müüb elektroonikat ja kodupaubandus в
+              Ассортимент интернет-магазина &quot;АкваТермикс&quot; включает в
               себя запасны.
             </p>
           </div>

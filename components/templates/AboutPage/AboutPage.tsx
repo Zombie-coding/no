f5973fe97eed47a6cb94b50816da7371f66de0b8
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-23 21:13:26 +0300

    Update AboutPage.tsx

--- components/templates/AboutPage/AboutPage.tsx
+++ components/templates/AboutPage/AboutPage.tsx
@@ -16,7 +16,7 @@ const AboutPage = () => {
         <div className={styles.about__inner}>
           <div className={`${styles.about__info} ${darkModeClass}`}>
             <p>
-              Ассортимент интернет-магазина &quot;АкваТермикс&quot;.
+              Ассортимент интернет-магазина &quot;АкваТермикс&quot;ddd.
             </p>
           </div>
           <div className={`${styles.about__img} ${styles.about__img__top}`}>

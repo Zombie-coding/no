c9379fa120d36e76b67f419a144c5aa7d64648f9
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-23 23:14:20 +0300

    Update AboutPage.tsx

--- components/templates/AboutPage/AboutPage.tsx
+++ components/templates/AboutPage/AboutPage.tsx
@@ -16,7 +16,6 @@ const AboutPage = () => {
         <div className={styles.about__inner}>
           <div className={`${styles.about__info} ${darkModeClass}`}>
             <p>
-              Ассортимент интернет-магазина &quot;АкваТермикс&quot; включает в
               себя запасные части для котлов Arderia, Ariston, Baxi, Beretta,
               Bosch, Buderus, Chaffoteaux, De Dietrich, Demrad, Electrolux,
               Ferroli, Fondital, Immergas, Junkers, Koreastar, Nova Florida,

abfd721d2d82d69830f6e23a987446b70ca38cce
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-23 17:53:23 +0300

    Update Footer.tsx

--- components/modules/Footer/Footer.tsx
+++ components/modules/Footer/Footer.tsx
@@ -129,6 +129,10 @@ const Footer = () => {
               </h3>
               <ul className={styles.footer__bottom__block__social}>
                 <li className={styles.footer__bottom__block__social__item}>
+                   <a
+                    href="#"
+                    className={styles.footer__bottom__block__social__item_vk}
+                  />
                 </li>
                 <li className={styles.footer__bottom__block__social__item}>
                   <a

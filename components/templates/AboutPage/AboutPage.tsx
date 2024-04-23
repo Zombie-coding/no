50aa370d58e5a410105d78a6da42c74890c0596d
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-23 20:27:27 +0300

    Update AboutPage.tsx

--- components/templates/AboutPage/AboutPage.tsx
+++ components/templates/AboutPage/AboutPage.tsx
@@ -12,9 +12,22 @@ const AboutPage = () => {
       <div className="container">
         <h2 className={`${styles.about__title} ${darkModeClass}`}>
           Ettevõttest
-        </h2>
+         </h2>
         <div className={styles.about__inner}>
           <div className={`${styles.about__info} ${darkModeClass}`}>
+            <p>
+              Компания &quot;АкваТермикс&quot; предлагает Вам запасные части для
+              европейских, корейских и отечественных газовых и электрических
+              котлов. 99% запчастей представленных на сайте постоянно
+              поддерживаются в наличии на нашем складе.
+            </p>
+            <p>
+              Ассортимент интернет-магазина &quot;АкваТермикс&quot; включает в
+              себя запасные части для котлов Arderia, Ariston, Baxi, Beretta,
+              Bosch, Buderus, Chaffoteaux, De Dietrich, Demrad, Electrolux,
+              Ferroli, Fondital, Immergas, Junkers, Koreastar, Nova Florida,
+              Saunier Duval, Sime, Tiberis, Vaillant, Viessmann, Westen.
+            </p>
           </div>
           <div className={`${styles.about__img} ${styles.about__img__top}`}>
             <img src="/img/about-img.png" alt="image-1" />

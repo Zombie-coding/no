b51df4cb2a17fad139431e83b4dd81636e15fc25
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-23 17:01:48 +0300

    Update CompanyContent.tsx

--- components/modules/Footer/CompanyContent.tsx
+++ components/modules/Footer/CompanyContent.tsx
@@ -18,7 +18,7 @@ const CompanyContent = () => (
     <li className={styles.footer__top__item__list__item}>
       <Link href="/wholesale-buyers" passHref legacyBehavior>
         <a className={styles.footer__top__item__list__item__link}>
-          Оптовым покупателям
+          ОHulgimüük kliendid
         </a>
       </Link>
     </li>

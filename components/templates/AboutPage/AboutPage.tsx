126d68b323bc6e306f83810e173e5bc655c7bc20
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-23 21:29:29 +0300

    Update AboutPage.tsx

--- components/templates/AboutPage/AboutPage.tsx
+++ components/templates/AboutPage/AboutPage.tsx
@@ -16,12 +16,6 @@ const AboutPage = () => {
         <div className={styles.about__inner}>
           <div className={`${styles.about__info} ${darkModeClass}`}>
             <p>
-              Компания &quot;АкваТермикс&quot; предлагает Вам запасные части для
-              европейских, корейских и отечественных газовых и электрических
-              котлов. 99% запчастей представленных на сайте постоянно
-              поддерживаются в наличии на нашем складе.
-            </p>
-            <p>
               Ассортимент интернет-магазина &quot;АкваТермикс&quot; включает в
               себя запасные части для котлов Arderia, Ariston, Baxi, Beretta,
               Bosch, Buderus, Chaffoteaux, De Dietrich, Demrad, Electrolux,

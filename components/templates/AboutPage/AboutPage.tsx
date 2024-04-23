bb5171d2a949b38b690215d0d932b0d9d33b79a1
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-23 19:00:57 +0300

    Update AboutPage.tsx

--- components/templates/AboutPage/AboutPage.tsx
+++ components/templates/AboutPage/AboutPage.tsx
@@ -19,7 +19,11 @@ const AboutPage = () => {
               Tere tulemast innovatsioonide maailma Vokinikoo juures! Meie valikust leiad laia valiku elektroonikat, varuosi ja kodutarbeid. Otsid siis viimast nutitelefoni, remondiosasid oma seadmele või funktsionaalseid esemeid koju, oleme siin, et aidata sul oma ideid ellu viia!.
             </p>
             <p>
-              Vokinikoo - teie usaldusväärne partner tehnoloogia ja kodumugavuste maailmas. Meie eesmärk on tagada meie klientidele kõik vajalik: alates kõige kaasaegsemast elektroonikast kuni usaldusväärsete varuosadeni ja stiilsete sisekujunduselementideni. Meie missioon on muuta teie elu lihtsamaks, mugavamaks ja põnevamaks. Usaldage meid ja saate juurdepääsu parimatele tehnoloogiatele ja kodusele mugavusele!
+              Ассортимент интернет-магазина &quot;АкваТермикс&quot; включает в
+              себя запасные части для котлов Arderia, Ariston, Baxi, Beretta,
+              Bosch, Buderus, Chaffoteaux, De Dietrich, Demrad, Electrolux,
+              Ferroli, Fondital, Immergas, Junkers, Koreastar, Nova Florida,
+              Saunier Duval, Sime, Tiberis, Vaillant, Viessmann, Westen.
             </p>
           </div>
           <div className={`${styles.about__img} ${styles.about__img__top}`}>

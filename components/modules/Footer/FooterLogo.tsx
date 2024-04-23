1b4a676dbdf2ce03cff89cb20e523ac2cecd2dc4
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-23 18:05:53 +0300

    Update FooterLogo.tsx

--- components/modules/Footer/FooterLogo.tsx
+++ components/modules/Footer/FooterLogo.tsx
@@ -8,7 +8,7 @@ const FooterLogo = () => (
       <a className={styles.footer__top__item__logo}>
         <img src="/img/logo-footer.svg" alt="logo" />
         <span className={styles.footer__top__item__logo__text}>
-          Детали для газовых котлов
+          Elektroonika ja Kodukaubandus
         </span>
       </a>
     </Link>

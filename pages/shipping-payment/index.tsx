4e40197b132b9bcf098b6a86168dd2d4975c626a
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-24 11:01:54 +0300

    add text

--- pages/shipping-payment/index.tsx
+++ pages/shipping-payment/index.tsx
@@ -5,13 +5,13 @@ import Breadcrumbs from '@/components/modules/Breadcrumbs/Breadcrumbs'
 import { useCallback } from 'react'
 
 function ShippingPaymentPage() {
-  const getDefaultTextGenerator = useCallback(() => 'Доставка и оплата', [])
+  const getDefaultTextGenerator = useCallback(() => 'Tarne ja maksmine', [])
   const getTextGenerator = useCallback((param: string) => ({}[param]), [])
 
   return (
     <>
       <Head>
-        <title>Аква Тепмикс | Доставка и оплата</title>
+        <title>Voiikoo | Tarne ja maksmine</title>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />

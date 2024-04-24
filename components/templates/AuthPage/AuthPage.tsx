3c50b00ff90fbc71321332dc9d51afa46b71dad9
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-24 08:44:39 +0300

    Update AuthPage.tsx

--- components/templates/AuthPage/AuthPage.tsx
+++ components/templates/AuthPage/AuthPage.tsx
@@ -82,7 +82,7 @@ const AuthPage = () => {
               <p
                 className={`${styles.switch__description} ${styles.description} ${darkModeClass}`}
               >
-                Чтобы оставаться Sisestage oma isikuandmed
+                ЧSisestage oma isikuandmed ja alustage oma teekonda koos 
               </p>
             </>
           )}

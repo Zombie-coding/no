b63251229ecfaffa1abec09c794e9cddf56e0d0b
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-24 12:50:39 +0300

    add api

--- app/api/geolocation.ts
+++ app/api/geolocation.ts
@@ -6,7 +6,7 @@ import api from '../axiosClient'
 export const getGeolocationFx = createEffect(
   async ({ latitude, longitude }: IGeolocation) => {
     const data = await api.get(
-      `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=ru&apiKey=${process.env.NEXT_PUBLIC_GEOAPI_KEY}`,
+      `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=ee&apiKey=${process.env.NEXT_PUBLIC_GEOAPI_KEY}`,
       { withCredentials: false }
     )
 

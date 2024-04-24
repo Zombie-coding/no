812f84428f44ee21646d4ab0ea1f55ed7b836003
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-24 12:11:57 +0300

    add text

--- components/templates/AboutPage/AboutPage.tsx
+++ components/templates/AboutPage/AboutPage.tsx
@@ -11,25 +11,13 @@ const AboutPage = () => {
     <section className={styles.about}>
       <div className="container">
         <h2 className={`${styles.about__title} ${darkModeClass}`}>
-          Ettevõttest
+          Ettevõtte sponsorid
         </h2>
         <div className={styles.about__inner}>
           <div className={`${styles.about__info} ${darkModeClass}`}>
             <p>
-              "Tere tulemast innovatsioonide maailma Vokinikoo juures! Meie
-              valikust leiad laia valiku elektroonikat, varuosi ja kodutarbeid.
-              Otsid siis viimast nutitelefoni, remondiosasid oma seadmele või
-              funktsionaalseid esemeid koju, oleme siin, et aidata sul oma ideid
-              ellu viia!"
-            </p>
-            <p>
-              Vokinikoo - teie usaldusväärne partner tehnoloogia ja
-              kodumugavuste maailmas. Meie eesmärk on tagada meie klientidele
-              kõik vajalik: alates kõige kaasaegsemast elektroonikast kuni
-              usaldusväärsete varuosadeni ja stiilsete sisekujunduselementideni.
-              Meie missioon on muuta teie elu lihtsamaks, mugavamaks ja
-              põnevamaks. Usaldage meid ja saate juurdepääsu parimatele
-              tehnoloogiatele ja kodusele mugavusele!"
+              Bosch, Buderus, Chaffoteaux, De Dietrich, Demrad, Electrolux,
+              Ferroli, Fondital, Immergas, Junkers, Koreastar, Nova Florida,
             </p>
           </div>
           <div className={`${styles.about__img} ${styles.about__img__top}`}>
